b996173623269034f7bb614ff2ad682974435618
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-10 07:21:27 +0200

    compose attributes into User class

---
+++ src/models/Attributes.ts
@@ -0,0 +1,13 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // types for keys of on abject. K is keyof T. IUserProps: { id, name, age}
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,15 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { rootUrl } from '../constants';
-
-export interface IUserProps {
-  id?: number;
-  name?: string;
-  age?: number;
-}
+import { IUserProps } from '../global';
+import { Attributes } from './Attributes';
 
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<IUserProps> = new Sync<IUserProps>(rootUrl);
+  public attribute: Attributes<IUserProps>;
 
-  constructor(private data: IUserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: IUserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: IUserProps) {
+    this.attributes = new Attributes<IUserProps>(attrs);
   }
 }
